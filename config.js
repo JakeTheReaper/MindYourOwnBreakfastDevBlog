109c287c4f5f131363689309f42cb012b2845543
JakeTheReaper <jake.zerafa86@gmail.com>
2019-09-14 21:47:10 +1000

    Update URL

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ module.exports = {
     linkedin: '',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://www.mindyourownbreakfast.com.au',
 };
