0b22fed653e2cb69d589786a97904e5a057f1833
JakeTheReaper <jake.zerafa86@gmail.com>
2019-09-14 21:41:34 +1000

    Initial Commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Mind Your Own Breakfast', // Required
+  author: 'Jake Zerafa', // Required
+  description: 'Web Developer',
+  primaryColor: '#6c12db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: '',
+    github: 'https://github.com/JakeTheReaper',
+    twitter: '',
+    linkedin: '',
+  },
+  pathPrefix: '/devblog',
+  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+};
